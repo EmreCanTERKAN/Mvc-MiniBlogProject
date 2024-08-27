22daaf503f20679954f20039497539efff5bbceb
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-27 17:18:38 +0300

    Add project files.

---
+++ WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        //veri tabanı olmadığı için buradan static olarak field tanımladık...
+        private static int _nextId = 1;
+        private static List<BlogPost> _posts = new List<BlogPost>();
+        public IActionResult Index()
+        {
+            ViewBag.TotalPost = _posts.Count;
+            ViewData["PageTitle"] = "Blog Gönderileri";
+
+
+            return View(_posts);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.CurrentTime = DateTime.Now;
+            return View();
+        }
+
+        //bunun blog tipi bir http post olacağını söyler.. 
+        [HttpPost]
+        public IActionResult Create(BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                blogPost.Id = _nextId++;
+                _posts.Add(blogPost);
+                TempData["SuccessMessage"] = "Blog Gönderisi Başarıyla Oluşturuldu";
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.CurrentTime = DateTime.Now;
+            return View(blogPost);
+
+        }
+
+        public IActionResult Details(int id)
+        {
+            var post = _posts.FirstOrDefault(x => x.Id == id);
+            if (post is null)
+            {
+                return NotFound();
+            }
+
+            ViewData["CreatedAgo"] = $"{(DateTime.Now - post.CreatedAt).TotalMinutes} dakika önce.";
+            return View(post);
+        }
+    }
+}
---
+++ WebApplication1/Models/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+
+        //Gerekli olan koşulları tanımlıyoruz..
+        [Required(ErrorMessage ="Başlık Gereklidir.")]
+        [StringLength(100,ErrorMessage = "100 karakterden fazla olamaz")]
+        public string Title { get; set; }
+        [Required(ErrorMessage ="İçerik Gereklidir..")]
+        public string Contant { get; set; }
+        // Oluşturulduğunda default olarak o anki zamanı vercektir
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
